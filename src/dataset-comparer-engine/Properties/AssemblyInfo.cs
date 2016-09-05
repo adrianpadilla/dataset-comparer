b90aee1cf7849d64bbd4d3a095bee47ad0e0acf8
Adrian <adrian@adrianpadilla.net>
2016-09-05 04:32:11 -0700

    changing the solution name

--- src/dataset-comparer-engine/Properties/AssemblyInfo.cs
+++ src/dataset-comparer-engine/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdrianPadillaNet.DataDiff")]
+[assembly: AssemblyTitle("dataset-comparer-engine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdrianPadillaNet.DataDiff")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("dataset-comparer-engine")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("579ae573-33ae-4b6b-b827-91016a07fef8")]
+[assembly: Guid("2164c4ae-1513-4d77-8e2d-8ec6645a4772")]
 
 // Version information for an assembly consists of the following four values:
 //
